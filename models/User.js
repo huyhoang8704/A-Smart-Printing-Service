2eb641d3fddef3a3b3db9e65f911be671eeb19f8
Nguyen Huy Hoang <137654109+huyhoang8704@users.noreply.github.com>
2024-10-30 03:21:19 +0700

    create model user

---
+++ models/User.js
@@ -0,0 +1,46 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/mysql.database";
+
+const User = sequelize.define('User',{
+    id : {
+        type : DataTypes.INTEGER,
+        autoIncrement : true, // Tự động tăng
+        allowNull : false,
+        primaryKey : true
+    },
+    fullname: {
+        type : DataTypes.STRING,
+        allowNull : false
+    }, 
+    username :{
+        type : DataTypes.STRING,
+        allowNull : false,
+        unique : true
+    },
+    password : {
+        type : DataTypes.STRING,
+        allowNull : false
+    },
+    numberPage : {
+        type : DataTypes.INTEGER
+    },
+    role: {
+        type: DataTypes.ENUM("student", "lecturer"),
+        allowNull: false,
+        defaultValue: "student" 
+    },
+    token : {
+        type : DataTypes.STRING,
+        allowNull : false
+    },
+    deleted : {
+        type : DataTypes.BOOLEAN,
+        defaultValue : false,
+    }
+
+}, {
+    tableName : 'users',
+    timestamps : true // createdAt, updatedAt
+})
+
+export default User
