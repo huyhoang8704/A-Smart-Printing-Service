083d8311f7e48c353cf0a853fbd3816b9fb482a4
Thai Nguyen <nh0kcrazy04@gmail.com>
2024-11-08 17:22:36 +0700

    newest

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,11 +1,10 @@
-const User = require('../models/User');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const dotenv = require('dotenv');
-const { v4: uuidv4 } = require('uuid');
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const dotenv = require("dotenv");
+const { v4: uuidv4 } = require("uuid");
 
 dotenv.config();
-
 
 const register = async (req, res) => {
     try {
@@ -14,7 +13,7 @@ const register = async (req, res) => {
         // Check existing email
         const existingUser = await User.findOne({ where: { email } });
         if (existingUser) {
-            return res.status(400).json({ message: 'Email đã được sử dụng.' });
+            return res.status(400).json({ message: "Email đã được sử dụng." });
         }
 
         // Hash password
@@ -27,17 +26,17 @@ const register = async (req, res) => {
             email,
             uniId,
             password: hashedPassword,
-            role: role || 'student',
+            role: role || "student",
         });
 
         // Create JWT token
         const token = jwt.sign(
-            { 
+            {
                 id: newUser.id,
                 fullName: newUser.fullName,
-            }, 
-            process.env.JWT_SECRET, 
-            { expiresIn: '1d' }
+            },
+            process.env.JWT_SECRET,
+            { expiresIn: "1d" }
         );
 
         // Save MySQL
@@ -47,23 +46,23 @@ const register = async (req, res) => {
         // Set cookie
         res.cookie("token", token, {
             maxAge: 24 * 60 * 60 * 1000,
-            httpOnly: true,  
-            secure: true     
+            httpOnly: true,
+            secure: true,
         });
 
         res.status(201).json({
-            message: 'Đăng ký thành công!',
+            message: "Đăng ký thành công!",
             user: {
                 id: newUser.id,
                 fullName: newUser.fullName,
                 email: newUser.email,
                 role: newUser.role,
-                uniId: newUser.uniId
+                uniId: newUser.uniId,
             },
-            token
+            token,
         });
     } catch (error) {
-        res.status(500).json({ message: 'Có lỗi xảy ra.', error: error.message });
+        res.status(500).json({ message: "Có lỗi xảy ra.", error: error.message });
     }
 };
 const login = async (req, res) => {
@@ -72,32 +71,32 @@ const login = async (req, res) => {
         const user = await User.findOne({ where: { email } });
         // Check Email
         if (!user) {
-            return res.status(400).json({ message: 'Email đăng nhập không chính xác.' });
+            return res.status(400).json({ message: "Email đăng nhập không chính xác." });
         }
         // Check Password
         const isPasswordValid = await bcrypt.compare(password, user.password);
         if (!isPasswordValid) {
-            return res.status(400).json({ message: 'Sai mật khẩu.' });
+            return res.status(400).json({ message: "Sai mật khẩu." });
         }
         // Create JWT token
         const token = jwt.sign(
-            { 
+            {
                 id: user.id,
                 fullName: user.fullName,
-            }, 
-            process.env.JWT_SECRET, 
-            { expiresIn: '1d' }
+            },
+            process.env.JWT_SECRET,
+            { expiresIn: "1d" }
         );
         // Set cookie
         res.cookie("token", token, {
             maxAge: 24 * 60 * 60 * 1000,
-            httpOnly: true,  
-            secure: true     
+            httpOnly: true,
+            secure: true,
         });
 
-
         res.status(200).json({
-            message: 'Đăng nhập thành công!',
+            status: "success",
+            message: "Đăng nhập thành công!",
             user: {
                 id: user.id,
                 fullName: user.fullName,
@@ -105,10 +104,10 @@ const login = async (req, res) => {
                 role: user.role,
                 uniId: user.uniId,
             },
-            token
+            token,
         });
     } catch (error) {
-        res.status(500).json({ message: 'Có lỗi xảy ra.', error: error.message });
+        res.status(500).json({ status: "failed", message: "Có lỗi xảy ra.", error: error.message });
     }
 };
 const getUser = async (req, res) => {
@@ -116,30 +115,30 @@ const getUser = async (req, res) => {
         const userId = req.params.id;
 
         const user = await User.findByPk(userId, {
-            attributes: ['id', 'fullName', 'email', 'role'] // Chỉ lấy các trường cần thiết
+            attributes: ["id", "fullName", "email", "role"], // Chỉ lấy các trường cần thiết
         });
 
         if (!user) {
-            return res.status(404).json({ message: 'Người dùng không tồn tại.' });
+            return res.status(404).json({ status: "failed", message: "Người dùng không tồn tại." });
         }
 
         res.status(200).json({
-            message: 'Lấy thông tin người dùng thành công!',
-            user
+            status: "success",
+            message: "Lấy thông tin người dùng thành công!",
+            user,
         });
     } catch (error) {
-        res.status(500).json({ message: 'Có lỗi xảy ra.', error: error.message });
+        res.status(500).json({ status: "failed", message: "Có lỗi xảy ra.", error: error.message });
     }
 };
 const logout = (req, res) => {
-    res.clearCookie('token'); 
-    res.status(200).json({ message: 'Đăng xuất thành công!' });
+    res.clearCookie("token");
+    res.status(200).json({ message: "Đăng xuất thành công!" });
 };
-
 
 module.exports = {
     register,
     login,
     getUser,
     logout,
-}
+};
