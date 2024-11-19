5c474ca1fd4f61dba083cf523cf7227f6ec5c0dc
Thai Nguyen <nh0kcrazy04@gmail.com>
2024-11-19 20:40:51 +0700

    add momo

--- controllers/pageBoughtController.js
+++ controllers/pageBoughtController.js
@@ -3,21 +3,32 @@ const pageBoughtService = require("../services/pageBoughtService");
 const { getCeilingNumber } = require("../utils/numberFormat");
 const crypto = require("crypto");
 
+async function result(req, res) {
+    console.log("this");
+    console.log(req.body);
+    res.status(204).send("OK bae");
+}
+
 async function buyPagesHandler(req, res) {
     var accessKey = "F8BBA842ECF85";
     var secretKey = "K951B6PE1waDMi640xX08PD3vg6EkVlz";
     var orderInfo = "pay with MoMo";
     var partnerCode = "MOMO";
-    var redirectUrl = "http://localhost:3001";
-    var ipnUrl = "http://localhost:3001";
+    var redirectUrl = "http://localhost:3001/page/result";
+    var ipnUrl = `${process.env.SERVER_URL}/page/result`;
     var requestType = "payWithMethod";
-    var amount = "50000";
+    var amount = "20000";
     var orderId = partnerCode + new Date().getTime();
     var requestId = orderId;
     var extraData = "";
+    var paymentCode =
+        "T8Qii53fAXyUftPV3m9ysyRhEanUs9KlOPfHgpMR0ON50U10Bh+vZdpJU7VY4z+Z2y77fJHkoDc69scwwzLuW5MzeUKTwPo3ZMaB29imm6YulqnWfTkgzqRaion+EuD7FN9wZ4aXE1+mRt0gHsU193y+yxtRgpmY7SDMU9hCKoQtYyHsfFR5FUAOAKMdw2fzQqpToei3rnaYvZuYaxolprm9+/+WIETnPUDlxCYOiw7vPeaaYQQH0BF0TxyU3zu36ODx980rJvPAgtJzH1gUrlxcSS1HQeQ9ZaVM1eOK/jl8KJm6ijOwErHGbgf/hVymUQG65rHU2MWz9U8QUjvDWA==";
     var orderGroupId = "";
     var autoCapture = true;
     var lang = "vi";
+
+    //before sign HMAC SHA256 with format
+    //accessKey=$accessKey&amount=$amount&extraData=$extraData&ipnUrl=$ipnUrl&orderId=$orderId&orderInfo=$orderInfo&partnerCode=$partnerCode&redirectUrl=$redirectUrl&requestId=$requestId&requestType=$requestType
     var rawSignature =
         "accessKey=" +
         accessKey +
@@ -43,10 +54,12 @@ async function buyPagesHandler(req, res) {
     console.log("--------------------RAW SIGNATURE----------------");
     console.log(rawSignature);
     //signature
+    const crypto = require("crypto");
     var signature = crypto.createHmac("sha256", secretKey).update(rawSignature).digest("hex");
     console.log("--------------------SIGNATURE----------------");
     console.log(signature);
 
+    //json object send to MoMo endpoint
     const requestBody = {
         partnerCode: partnerCode,
         partnerName: "Test",
@@ -133,3 +146,4 @@ exports.buyPagesHandler = buyPagesHandler;
 exports.viewPageBoughtHistory = viewPageBoughtHistory;
 exports.requestBuyPagesHandler = requestBuyPagesHandler;
 exports.updateOrderHandler = updateOrderHandler;
+exports.result = result;
--- routes/pageBoughtRoute.js
+++ routes/pageBoughtRoute.js
@@ -3,8 +3,12 @@ const router = Router();
 const pageBoughtHandler = require("../controllers/pageBoughtController");
 const { authenticateBearerToken } = require("../middlewares/authenticate.middleware");
 
+
+router.get("/result", pageBoughtHandler.result);
+router.post("/result", pageBoughtHandler.result);
 router.use(authenticateBearerToken);
 
+router.post("/test-momo", pageBoughtHandler.buyPagesHandler);
 router.post("/buy", pageBoughtHandler.requestBuyPagesHandler);
 router.patch("/buy", pageBoughtHandler.updateOrderHandler);
 router.get("/history", pageBoughtHandler.viewPageBoughtHistory);
