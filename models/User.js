991ce10a2ba4c53520f4903374bd488485a328f2
Thai Nguyen <nh0kcrazy04@gmail.com>
2024-11-22 09:04:45 +0700

    default value = 0 for pages

--- models/User.js
+++ models/User.js
@@ -1,54 +1,58 @@
-const DataTypes = require("sequelize")
-const sequelize = require("../config/mysql.database")
+const DataTypes = require("sequelize");
+const sequelize = require("../config/mysql.database");
 
-const User = sequelize.define('User',{
-    id: {
-        type: DataTypes.STRING(36),
-        allowNull: false,
-        primaryKey: true
-      },
-    uniId: {
-        type: DataTypes.STRING(7),
-        allowNull: false,
+const User = sequelize.define(
+    "User",
+    {
+        id: {
+            type: DataTypes.STRING(36),
+            allowNull: false,
+            primaryKey: true,
+        },
+        uniId: {
+            type: DataTypes.STRING(7),
+            allowNull: false,
+        },
+        fullName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        numberPage: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0,
+        },
+        role: {
+            type: DataTypes.ENUM("student", "lecturer"),
+            allowNull: false,
+            defaultValue: "student",
+        },
+        token: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        lastSemPaperReceive: {
+            type: DataTypes.STRING,
+        },
+        deleted: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+        },
     },
-    fullName: {
-        type : DataTypes.STRING,
-        allowNull : false
-    }, 
-    email :{
-        type : DataTypes.STRING,
-        allowNull : false,
-        unique : true
-    },
-    password : {
-        type : DataTypes.STRING,
-        allowNull : false
-    },
-    numberPage : {
-        type : DataTypes.INTEGER
-    },
-    role: {
-        type: DataTypes.ENUM("student", "lecturer"),
-        allowNull: false,
-        defaultValue: "student" 
-    },
-    token : {
-        type : DataTypes.STRING,
-        allowNull : true
-    },
-    lastSemPaperReceive: {
-        type : DataTypes.STRING,
-    },
-    deleted : {
-        type : DataTypes.BOOLEAN,
-        defaultValue : false,
+    {
+        tableName: "users",
+        timestamps: true, // createdAt, updatedAt
     }
-
-}, {
-    tableName : 'users',
-    timestamps : true // createdAt, updatedAt
-})
+);
 
 // User.sync({alter: true})
 
-module.exports = User
+module.exports = User;
